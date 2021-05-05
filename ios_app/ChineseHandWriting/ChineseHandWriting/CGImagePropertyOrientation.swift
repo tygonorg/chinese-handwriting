d1e916cb2763fb26aeca9fc29bdd20ed585329cc
Tran Phuoc Hung <tphunghue@hotmail.com>
2021-05-06 00:29:09 +0700

    add drawcanvas

---
+++ ios_app/ChineseHandWriting/ChineseHandWriting/CGImagePropertyOrientation.swift
@@ -0,0 +1,32 @@
+//
+//  CGImagePropertyOrientation.swift
+//  ChineseHandWriting
+//
+//  Created by Hung Tran on 5/5/21.
+//
+
+import UIKit
+import ImageIO
+extension CGImagePropertyOrientation {
+    /**
+     Converts a `UIImageOrientation` to a corresponding
+     `CGImagePropertyOrientation`. The cases for each
+     orientation are represented by different raw values.
+     
+     - Tag: ConvertOrientation
+     */
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            fatalError()
+        }
+    }
+}
--- ios_app/ChineseHandWriting/ChineseHandWriting/ChineseHandWrittingViewController.swift
+++ ios_app/ChineseHandWriting/ChineseHandWriting/ChineseHandWrittingViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 enum PenType {
     case pen,eraser
 }
@@ -18,13 +20,108 @@ class ChineseHandWrittingViewController: UIViewController {
     var swiped = false
     var type : PenType = .pen
     
+    @IBOutlet weak var btn0: UIButton!
+    
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    /// - Tag: MLModelSetup
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: cl().model)
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        // classificationLabel.text = "Classifying..."
+        
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async { [self] in
+            guard let results = request.results else {
+                //  self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+            
+            if classifications.isEmpty {
+                //  self.classificationLabel.text = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(5)
+                self.hideSugess()
+                for (index, value) in topClassifications.enumerated() {
+                    print("\(index): \(value)")
+                    if(index == 0){
+                        btn0.setTitle(value.identifier, for: .normal)
+                        btn0.isHidden = false
+                    }
+                    if(index == 1){
+                        btn1.setTitle(value.identifier, for: .normal)
+                        btn1.isHidden = false
+                    }
+                    if(index == 2){
+                        btn2.setTitle(value.identifier, for: .normal)
+                        btn2.isHidden = false
+                    }
+                    if(index == 3){
+                        btn3.setTitle(value.identifier, for: .normal)
+                        btn3.isHidden = false
+                    }
+                    if(index == 4){
+                        btn4.setTitle(value.identifier, for: .normal)
+                        btn4.isHidden = false
+                    }
+                }
+            }
+        }
+    }
+    
+    
     @IBOutlet weak var eraser_bottom: NSLayoutConstraint!
     @IBOutlet weak var pen_bottom: NSLayoutConstraint!
-    @IBOutlet weak var tempImageView: UIImageView!
+    @IBOutlet weak var drawCanvasView: UIImageView!
+    fileprivate func hideSugess() {
+        btn0.isHidden = true
+        btn1.isHidden = true
+        btn2.isHidden = true
+        btn3.isHidden = true
+        btn4.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUpUI()
+        hideSugess()
     }
+    
     func setUpUI() {
         switch type {
         case .pen:
@@ -55,16 +152,30 @@ class ChineseHandWrittingViewController: UIViewController {
         guard let touch = touches.first else {
             return
         }
-        swiped = false
-        lastPoint = touch.location(in: tempImageView)
+       
+
+        let p =  touch.location(in: drawCanvasView)
+        let q =  touch.location(in: self.view)
+        if drawCanvasView.frame.contains(q){
+            swiped = false
+            lastPoint = p
+        } else {
+            
+            print("tap out side image")
+            return
+        }
     }
+    
+    
+    
+    
     func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
         // 1
-        UIGraphicsBeginImageContext(tempImageView.frame.size)
+        UIGraphicsBeginImageContext(drawCanvasView.frame.size)
         guard let context = UIGraphicsGetCurrentContext() else {
             return
         }
-        tempImageView.image?.draw(in: tempImageView.bounds)
+        drawCanvasView.image?.draw(in: drawCanvasView.bounds)
         
         // 2
         context.move(to: fromPoint)
@@ -80,8 +191,8 @@ class ChineseHandWrittingViewController: UIViewController {
         context.strokePath()
         
         // 5
-        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        tempImageView.alpha = opacity
+        drawCanvasView.image = UIGraphicsGetImageFromCurrentImageContext()
+        drawCanvasView.alpha = opacity
         UIGraphicsEndImageContext()
     }
     
@@ -89,31 +200,42 @@ class ChineseHandWrittingViewController: UIViewController {
         guard let touch = touches.first else {
             return
         }
+        let currentPoint = touch.location(in: drawCanvasView)
+        let q =  touch.location(in: self.view)
         
-        // 6
+        if !drawCanvasView.frame.contains(q) {
+            print("move out side")
+            return
+        }
         swiped = true
-        let currentPoint = touch.location(in: tempImageView)
         drawLine(from: lastPoint, to: currentPoint)
-        
-        // 7
         lastPoint = currentPoint
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-      if !swiped {
-        // draw a single point
-        drawLine(from: lastPoint, to: lastPoint)
-      }
-        
-      // Merge tempImageView into mainImageView
-//      UIGraphicsBeginImageContext(mainImageView.frame.size)
-//      mainImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
-//      tempImageView?.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
-//      mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-//      UIGraphicsEndImageContext()
-//
-//      tempImageView.image = nil
+        if !swiped {
+            // draw a single point
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        guard let touch = touches.first else {
+            return
+        }
+        let q =  touch.location(in: self.view)
+      //  let currentPoint = touch.location(in: tempImageView)
+        if !drawCanvasView.frame.contains(q) {
+            // if(tempImageView.bounds.contains(p)){
+            print("tap end out side!")
+            return
+           // lastPoint = touch.location(in: tempImageView)
+        }
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(drawCanvasView.frame.size)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        updateClassifications(for: image!)
+        //
+        //      tempImageView.image = nil
     }
-
+    
     
     
 }
